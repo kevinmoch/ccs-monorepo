e88ea70cf0da05a9f9d83787ac693e1d0dbec328
kevinmoch <kevinmoch@qq.com>
2026-06-01 11:23:51 +0800

    feat: seperate homepage

--- apps/ccs-module-demo/src/App.vue
+++ apps/ccs-module-demo/src/App.vue
@@ -1,16 +1,3 @@
 <template>
-  <div class="module-shell">
-    <header class="module-header">
-      <div>
-        <span>ccs-module-demo</span>
-        <h2>{{ $t('demoModule') }}</h2>
-      </div>
-      <nav>
-        <RouterLink to="/">{{ $t('demoModule') }}</RouterLink>
-        <RouterLink to="/dashboard">{{ $t('demoDashboard') }}</RouterLink>
-        <!-- ccs-cli:nav -->
-      </nav>
-    </header>
-    <RouterView />
-  </div>
+  <RouterView />
 </template>
--- templates/module-vue/src/App.vue.tpl
+++ templates/module-vue/src/App.vue.tpl
@@ -1,15 +1,3 @@
 <template>
-	<div class="module-shell">
-		<header class="module-header">
-			<div>
-				<span>__MODULE_NAME__</span>
-				<h2>__MODULE_TITLE__</h2>
-			</div>
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<!-- ccs-cli:nav -->
-			</nav>
-		</header>
-		<RouterView />
-	</div>
+	<RouterView />
 </template>
